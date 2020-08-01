45852d0174a427635e543b91816c78f8f427f670
wabilin <twelendil@protonmail.com>
2020-08-01 13:18:13 +0900

    Add scheduleUpdater

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 export { default as telegramBot } from './telegramBot'
+export { default as scheduleUpdater } from './scheduleUpdater'
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
---
+++ functions/src/scheduleUpdater.ts
@@ -0,0 +1,24 @@
+import * as functions from 'firebase-functions';
+import parseScheduleHtml from 'holo-schedule'
+import getScheduleHtml from 'holo-schedule/lib/getScheduleHtml'
+
+import admin = require('firebase-admin');
+
+const scheduleUpdater = functions.pubsub.schedule('every 1 hours').onRun(async (context) => {
+  admin.initializeApp();
+  const db = admin.firestore();
+
+  const html = await getScheduleHtml()
+
+  const dictRef = db.collection('docs').doc('streamerImageDict');
+
+  const doc = await dictRef.get()
+  const streamerImageDict = doc.exists ? doc.data() : {}
+
+  const { dict } = parseScheduleHtml(html, streamerImageDict)
+  await dictRef.set(dict)
+
+  return null;
+});
+
+export default scheduleUpdater
