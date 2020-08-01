5f02ef86df6899fe385696aef046c9029dbf46e5
wabilin <twelendil@protonmail.com>
2020-08-01 11:52:08 +0900

    hello telbot

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,25 @@
 import * as functions from 'firebase-functions';
+import { Telegraf } from "telegraf";
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+const token: string = functions.config().bot.token;
+const username: string = functions.config().bot.username;
+const bot = new Telegraf(token, { username });
+
+bot.start((ctx) => ctx.reply("Welcome!"));
+bot.command("test", (ctx) => ctx.reply("Hi"));
+
+export const helloBot = functions.https.onRequest(async (request, response) => {
+    // Catch all workaround
+  bot.on('message', () => {
+    response.status(200).end()
+  })
+
+  try {
+    await bot.handleUpdate(request.body, response);
+  } catch (err) {
+    console.error(err);
+  }
 });
