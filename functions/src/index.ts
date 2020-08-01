4ce3daf4be72986252b26a7965b4ea52ad2e993e
wabilin <twelendil@protonmail.com>
2020-08-01 12:23:01 +0900

    log chat id

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,7 +9,12 @@ const username: string = functions.config().bot.username;
 const bot = new Telegraf(token, { username });
 
 bot.start((ctx) => ctx.reply("Welcome!"));
-bot.command("test", (ctx) => ctx.reply("Hi"));
+bot.command("test", (ctx) => {
+  const chatId = ctx.chat?.id
+  functions.logger.log(`Test command from chatId: ${chatId}`)
+
+  return ctx.reply("Hi")
+});
 
 export const helloBot = functions.https.onRequest(async (request, response) => {
     // Catch all workaround
