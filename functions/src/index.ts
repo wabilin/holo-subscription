c378477bc1802809c74b72fe45b87af7c513a727
wabilin <twelendil@protonmail.com>
2020-08-02 18:45:32 +0900

    Remove test trigger

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,6 @@
 export { default as telegramBot } from './telegramBot'
 export { default as scheduleUpdater } from './scheduleUpdater'
 export { default as notifyNewLive } from './notifyNewLive'
-export { default as testTriggerByDb } from './testTriggerByDb'
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
--- functions/src/testTriggerByDb.ts
+++
@@ -1,9 +0,0 @@
-import * as functions from 'firebase-functions';
-
-const testTriggerByDb = functions.firestore.document("schedule/{key}").onWrite((change, context) => {
-  functions.logger.log(`context.params: ${JSON.stringify(context.params)}`)
-  functions.logger.log(`before data: ${JSON.stringify(change.before.data())}`)
-  functions.logger.log(`after data: ${JSON.stringify(change.after.data())}`)
-});
-
-export default testTriggerByDb
