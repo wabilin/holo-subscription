5a6cceb3992b9c33521ed13adb197f30a6566783
wabilin <twelendil@protonmail.com>
2020-08-03 12:04:28 +0900

    fix halfHour

--- functions/src/checkIncomingLive.ts
+++ functions/src/checkIncomingLive.ts
@@ -6,7 +6,12 @@ import { ScheduleItem } from './types'
 const checkIncomingLive = functions.pubsub.schedule('every 10 minutes').onRun(async (context) => {
   const now = new Date()
   // FIXME: 300 -> 30 after debug done
-  const halfHourLater = (new Date(now)).setMinutes(now.getMinutes() + 300)
+  const halfHourLater = new Date(now)
+  halfHourLater.setMinutes(now.getMinutes() + 300)
+
+  functions.logger.log('now: ', now)
+  functions.logger.log('halfHourLater: ', halfHourLater)
+
   const ref = getScheduleRef().where('time', '>', now).where('time', '<', halfHourLater)
   const scheduleItems = await ref.get()
 
@@ -15,6 +20,7 @@ const checkIncomingLive = functions.pubsub.schedule('every 10 minutes').onRun(as
     const item = x.data() as ScheduleItem
     lives.push(item)
   })
+  functions.logger.log('lives added in incoming:', lives)
 
   return createIncomingNotifications(lives)
 });
