3f17b9cea53856195ff1117a2950fb535a7cb7d4
wabilin <twelendil@protonmail.com>
2020-08-03 13:06:02 +0900

    checking incoming: 30 mins

--- functions/src/checkIncomingLive.ts
+++ functions/src/checkIncomingLive.ts
@@ -5,12 +5,8 @@ import { ScheduleItem } from './types'
 
 const checkIncomingLive = functions.pubsub.schedule('every 10 minutes').onRun(async (context) => {
   const now = new Date()
-  // FIXME: 300 -> 30 after debug done
   const halfHourLater = new Date(now)
-  halfHourLater.setMinutes(now.getMinutes() + 300)
-
-  functions.logger.log('now: ', now)
-  functions.logger.log('halfHourLater: ', halfHourLater)
+  halfHourLater.setMinutes(now.getMinutes() + 30)
 
   const ref = getScheduleRef().where('time', '>', now).where('time', '<', halfHourLater)
   const scheduleItems = await ref.get()
@@ -20,7 +16,6 @@ const checkIncomingLive = functions.pubsub.schedule('every 10 minutes').onRun(as
     const item = x.data() as ScheduleItem
     lives.push(item)
   })
-  functions.logger.log('lives added in incoming:', lives)
 
   return createIncomingNotifications(lives)
 });
