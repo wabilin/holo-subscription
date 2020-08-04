994e1fb2e3870cde0a0630b268f6a93f6394aa60
wabilin <twelendil@protonmail.com>
2020-08-04 23:59:00 +0900

    fix clearOldDate schedule

--- functions/src/clearOldData.ts
+++ functions/src/clearOldData.ts
@@ -3,7 +3,7 @@ import * as functions from 'firebase-functions';
 
 import { clearOldDbData } from "./util/db";
 
-const clearOldData = functions.pubsub.schedule('every 1 days').onRun((context) => {
+const clearOldData = functions.pubsub.schedule('44 1 * * *').timeZone('Asia/Tokyo').onRun((context) => {
   return clearOldDbData()
 });
 
