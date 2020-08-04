0f30a35cae05d549821b21099209795bbcb74173
wabilin <twelendil@protonmail.com>
2020-08-04 23:48:41 +0900

    add clearOldDate

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,8 @@ export { default as scheduleUpdater } from './scheduleUpdater'
 export { default as notifyNewLive } from './notifyNewLive'
 export { default as notifyIncomingLive } from './notifyIncomingLive'
 export { default as checkIncomingLive } from './checkIncomingLive'
+export { default as clearOldData } from './clearOldData'
+
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
