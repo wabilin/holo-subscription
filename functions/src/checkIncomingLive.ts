676776d2bf61221353dd7be9863ac8072b3c8d22
wabilin <twelendil@protonmail.com>
2020-08-04 22:31:07 +0900

    time use moment

--- functions/src/checkIncomingLive.ts
+++ functions/src/checkIncomingLive.ts
@@ -1,12 +1,12 @@
 import * as functions from 'firebase-functions';
+import * as moment from 'moment-timezone'
 
 import { getScheduleRef, createIncomingNotifications } from "./util/db";
 import { ScheduleItem } from './types'
 
 const checkIncomingLive = functions.pubsub.schedule('every 10 minutes').onRun(async (context) => {
   const now = new Date()
-  const halfHourLater = new Date(now)
-  halfHourLater.setMinutes(now.getMinutes() + 30)
+  const halfHourLater: Date = moment().add(30, 'minutes').toDate()
 
   const ref = getScheduleRef().where('time', '>', now).where('time', '<', halfHourLater)
   const scheduleItems = await ref.get()
